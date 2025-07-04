7e4ee9559f15a9db9d720a8e7082f1a171ff401a
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-07-04 15:56:38 +0700

    initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,65 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { base } from '@numbers/utils/base';
+import { NextPage } from 'next';
+import { useState } from 'react';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const INITIAL_NUMBER: number = 10;
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function Home() {
+const HomePage: NextPage = () => {
+  const [
+    {
+      base2 = base(INITIAL_NUMBER).from(10).to(2),
+      base8 = base(INITIAL_NUMBER).from(10).to(8),
+      base10 = INITIAL_NUMBER.toString(),
+      base16 = base(INITIAL_NUMBER).from(10).to(16),
+    },
+    setState,
+  ] = useState<{
+    base2: string;
+    base8: string;
+    base10: string;
+    base16: string;
+  }>({
+    base2: base(INITIAL_NUMBER).from(10).to(2),
+    base8: base(INITIAL_NUMBER).from(10).to(8),
+    base10: INITIAL_NUMBER.toString(),
+    base16: base(INITIAL_NUMBER).from(10).to(16),
+  });
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-y-2 divide-y divide-neutral-200 rounded border border-neutral-200 shadow-sm">
+        {[
+          { fromBase: 2, value: base2 },
+          { fromBase: 8, value: base8 },
+          { fromBase: 10, value: base10 },
+          { fromBase: 16, value: base16 },
+        ].map(({ fromBase, value }) => {
+          return (
+            <div className="flex items-center justify-center gap-x-2 px-4 py-2">
+              <span>Base {fromBase}</span>
+              <input
+                type="text"
+                id={`base${fromBase}`}
+                placeholder={`Base ${fromBase}`}
+                value={value}
+                onChange={(event) => {
+                  const newValue = event.target.value;
+                  const newNumber: number = parseInt(newValue);
+                  setState((previous) => ({
+                    ...previous,
+                    base2: base(newNumber).from(fromBase).to(2),
+                    base8: base(newNumber).from(fromBase).to(8),
+                    base10: base(newNumber).from(fromBase).to(10),
+                    base16: base(newNumber).from(fromBase).to(16),
+                  }));
+                }}
+                className="grow text-right focus:outline-none"
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
+
+export default HomePage;
